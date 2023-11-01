dfa5190096054582f7988161b18ba14c7802524a
fadialsalti <eng_fadi77@hotmail.com>
2023-11-01 21:37:03 +0100

    all done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
+app.jinja_env.add_extension('jinja2.ext.do')
 
 def change(amount):
     # calculate the resultant change and store the result (res)
@@ -50,4 +51,4 @@ def change100route(dollar, cents):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
